d3498c909c592761e43a84fc9dadc42d06982c1a
Austina <n1086722@ntub.edt.tw>
2019-12-06 20:48:57 +0800

    austina

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NewsModule } from './news/news.module';
+import { StudentModule } from './student/student.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [NewsModule, StudentModule, UserModule],
+  controllers: [AppController,],  
+  providers: [AppService,],
 })
 export class AppModule {}
